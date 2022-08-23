df7de6620800dea494faa14afc0213b876af6c41
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-23 07:40:14 -0500

    feat: nameSpaces, exports

--- bases/main.js
+++ bases/main.js
@@ -1,91 +1,12 @@
 "use strict";
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelerar();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelerar() {
-        console.log("...... gogogo!!!");
-    }
-};
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor(nombre, edad, sexo, estadoCivil) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
-    }
-    imprimirBio() {
-        console.log(`${this.nombre} tiene ${this.edad} años, y esta ${this.estadoCivil}`);
-    }
-}
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super speed', 'Time travel']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let spiderman = {
-        name: 'Barry Allen',
-        powers: ['Super speed', 'Time travel'],
-        getName() {
-            return this.name;
-        }
+    const validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return `${this.name} ${this.realName}`;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Salome',
-        age: 22,
-        address: {
-            id: 124,
-            zip: 'JD2 DAS',
-            city: 'Otawa'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumberFucntion;
-    addNumberFucntion = (a, b) => {
-        return a + b;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Salome'));
 //# sourceMappingURL=main.js.map
---
+++ ts-webpack/src/index.ts
@@ -0,0 +1,16 @@
+import * as HeroClasses from "./classes/Hero";
+import powers, { Power } from './data/powers';
+
+console.log('Hola Mundo!');
+
+const ironman = new HeroClasses.Hero('Ironman', 32, 23);
+
+console.log(ironman);
+
+console.log({ powers });
+
+console.log(ironman.power);
+
+
+
+
